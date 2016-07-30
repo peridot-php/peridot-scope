09d2a828053c902e98f0233888cb1bfe35fabd8e
Erin Millard <ezzatron@gmail.com>
2016-07-30 19:59:56 +1000

    Fixed undefined index errors.

--- peridot.php
+++ peridot.php
@@ -1,16 +1,37 @@
 <?php
 
 use Evenement\EventEmitterInterface;
+use Peridot\Concurrency\ConcurrencyPlugin;
+use Peridot\Console\Environment;
 use Peridot\Plugin\Watcher\WatcherPlugin;
 use Peridot\Reporter\CodeCoverageReporters;
 use Peridot\Reporter\Dot\DotReporterPlugin;
 use Peridot\Reporter\ListReporter\ListReporterPlugin;
+use Symfony\Component\Console\Input\InputInterface;
 
-return function(EventEmitterInterface $emitter) {
-    $watcher = new WatcherPlugin($emitter);
-    $dot = new DotReporterPlugin($emitter);
-    $list = new ListReporterPlugin($emitter);
+error_reporting(-1);
+
+return function (EventEmitterInterface $emitter) {
+    new WatcherPlugin($emitter);
+    new DotReporterPlugin($emitter);
+    new ListReporterPlugin($emitter);
+    new ConcurrencyPlugin($emitter);
+
+    // set the default path
+    $emitter->on('peridot.start', function (Environment $environment) {
+        $environment->getDefinition()
+            ->getArgument('path')->setDefault('specs');
+    });
+
+    // disable watcher for concurrency workers
+    $emitter->on('peridot.execute', function (InputInterface $input) {
+        if (getenv('PERIDOT_TEST_TOKEN')) {
+            $input->setOption('watch', false);
+        }
+    });
 
     $coverage = new CodeCoverageReporters($emitter);
     $coverage->register();
+
+    //$prophecy = new ProphecyPlugin($emitter);
 };
